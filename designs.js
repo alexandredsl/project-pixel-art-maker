19721ed76e985575242a20fea5a0162a50cc4f32
Alexandre Lacerda <alexandredsl@live.com>
2018-09-27 23:19:25 -0300

    Project finished.

--- designs.js
+++ designs.js
@@ -2,23 +2,38 @@
 
 // TODO: definição de váriaveis;
 
-let pixelCanvas = document.querySelector("#pixelCanvas");
+const pixelCanvas = document.getElementById('pixelCanvas');
 
-let currentColor = document.querySelector("#colorPicker").value;
+let currentColor = document.getElementById("colorPicker").value;
 
-let gridHeight = document.querySelector("#inputHeight").value;
+let gridHeight;
 
-let gridWidth = document.querySelector("#inputWidth").value;
+let gridWidth;
 
-const submitButton = document.querySelector("#submit");
+const submitButton = document.getElementById("submit");
 
 // Select color input
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
+function makeGrid(event) {
+  $(pixelCanvas).empty();
+  event.preventDefault();
+  pixelCanvas.createTBody();
+  const tableBody = pixelCanvas.firstElementChild;
+  gridHeight = document.getElementById("inputHeight").value;
+  gridWidth = document.getElementById("inputWidth").value;
+  for (let itr = 0; gridHeight > itr; itr++) {
+    tableBody.insertRow(itr)
+    for (let itr2 = 0; gridWidth > itr2; itr2++) {
+      tableBody.children[itr].insertCell(0);
+    }
+    $("td").click(function(event){
+      let currentCell = event.target;
+      currentCell.style.backgroundColor = colorPicker.value;
+    })
+  };
 
 }
+$(submitButton).click(makeGrid);
